f644a8335fdfdd4ad47dee184f7dd6b21edfe0fa
k.noskov <k.noskov@lancelot.prod>
2022-02-22 21:07:02 +0300

    Update shell

--- src/main/resources/saml/generate-key.sh
+++ src/main/resources/saml/generate-key.sh
@@ -1,2 +1,6 @@
 
-keytool -keyalg RSA -keysize 2048 -genkeypair -alias apollo -keypass nalle123 -keystore saml-keystore.jks
+keytool -keyalg RSA -keysize 2048 -genkeypair -alias apollo -keypass nalle123 -keystore saml-keystore.jks #Generate key store
+
+keytool -export -keystore saml-keystore.jks -alias apollo -file apollo.pem -rfc #Get Signature Certificate
+
+keytool -import -alias okta -file okta.cert -keystore saml-keystore.jks -storepass nalle123 -noprompt #???
